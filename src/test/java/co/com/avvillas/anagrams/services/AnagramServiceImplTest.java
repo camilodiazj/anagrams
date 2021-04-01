ea4715ebc41678a6c8796512ad7afd7fedb6c3db
Camilo Diaz <camilo.diaz@avaldigitallabs.com>
2021-04-01 17:09:44 -0500

    :white_check_mark: Added first test

--- src/test/java/co/com/avvillas/anagrams/services/AnagramServiceImplTest.java
+++ src/test/java/co/com/avvillas/anagrams/services/AnagramServiceImplTest.java
@@ -1,9 +1,22 @@
 package co.com.avvillas.anagrams.services;
 
 import co.com.avvillas.anagrams.service.AnagramServiceImpl;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.MockitoJUnitRunner;
 
+@RunWith(MockitoJUnitRunner.class)
 public class AnagramServiceImplTest {
 
+  @InjectMocks
   private AnagramServiceImpl anagramService;
+
+  @Test
+  public void shouldAreWordsAnagramsReturnTrue(){
+    boolean result = anagramService.areWordsAnagrams("Mary", "army");
+    Assert.assertFalse(result);
+  }
 
 }
