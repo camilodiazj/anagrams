daec056aa3acc2995bafc97c2dfda439be543bcb
Camilo Diaz <camilo.diaz@avaldigitallabs.com>
2021-04-01 09:50:39 -0500

    :tada: base project

---
+++ src/main/java/co/com/avvillas/anagrams/AnagramsApplication.java
@@ -0,0 +1,13 @@
+package co.com.avvillas.anagrams;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnagramsApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(AnagramsApplication.class, args);
+  }
+
+}
---
+++ src/test/java/co/com/avvillas/anagrams/AnagramsApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.avvillas.anagrams;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnagramsApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
