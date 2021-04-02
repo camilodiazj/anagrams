3d30103ee889bd7d547960749ae69d107349cca5
Camilo Diaz <camilo.diaz@avaldigitallabs.com>
2021-04-01 21:24:11 -0500

    :construction: Added Swagger

--- src/main/java/co/com/avvillas/anagrams/AnagramsApplication.java
+++ src/main/java/co/com/avvillas/anagrams/AnagramsApplication.java
@@ -2,8 +2,10 @@ package co.com.avvillas.anagrams;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class AnagramsApplication {
 
   public static void main(String[] args) {
---
+++ src/main/java/co/com/avvillas/anagrams/configuration/SwaggerConfig.java
@@ -0,0 +1,18 @@
+package co.com.avvillas.anagrams.configuration;
+
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+public class SwaggerConfig {
+
+  @Bean
+  public Docket productApi() {
+    return new Docket(DocumentationType.SWAGGER_2)
+        .select()
+        .apis(RequestHandlerSelectors.basePackage("co.com.avvillas.anagrams"))
+        .build();
+  }
+
+}
