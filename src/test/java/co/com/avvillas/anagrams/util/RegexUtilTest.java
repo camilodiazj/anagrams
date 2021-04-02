6f71903f8234098a3ec31ec8302b3fae844f3749
Camilo Diaz <camilo.diaz@avaldigitallabs.com>
2021-04-02 11:35:28 -0500

    :white_check_mark: Created more UT

---
+++ src/test/java/co/com/avvillas/anagrams/util/RegexUtilTest.java
@@ -0,0 +1,19 @@
+package co.com.avvillas.anagrams.util;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class RegexUtilTest {
+
+  @Test
+  public void shouldWordMatchRegexReturnTrue(){
+    boolean result = RegexUtils.wordMatchRegex("camilo", RegexUtils.ONLY_LETTERS_REGEX);
+    Assert.assertTrue(result);
+  }
+
+  @Test
+  public void shouldWordMatchRegexReturnFalse(){
+    boolean result = RegexUtils.wordMatchRegex("cam7ilo", RegexUtils.ONLY_LETTERS_REGEX);
+    Assert.assertFalse(result);
+  }
+}
